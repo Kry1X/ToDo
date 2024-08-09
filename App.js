18cb80d110f378465b9c44c739959db980d8592e
Kry1X <bogdankrylosov08@gmail.com>
2024-08-09 18:01:28 +0500

    Add CRUD functions, but they dont work

--- App.js
+++ App.js
@@ -1,33 +1,56 @@
 import { useState } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
 import Element from './components/ListElement';
 import FormCreate from './components/FormCreate';
 import Settings from './components/Settings';
 import Favorites from './components/Favorites';
-// ToDo: тест всех окон(+Избранное, +Основной список, +Настройки, +Создание) в app.js
+
 export default function App() {
-  const [listOfItems] = useState([
-    {text: "borderRadius\nA\nB\nC", key: "1"},
-    {text: "Todo_2", key: "2"},
-    {text: "Todo_3", key: "3"},
-  ]) 
+  const [listOfItems, setListOfItems] = useState([
+    {text:"awd", fav:false, key: "3"},
+    {text:"qwe", fav:false, key: "2"},
+  ]) // {text:"", fav:t|f, key: Math.random().toString(36).substring(7)} 
+
+  const addItem = (data) => {
+    setListOfItems((list) => {
+      return [
+        {text: data.text, fav: data.fav, key: data.key},//Math.random().toString(36).substring(7)
+        ...list
+      ]
+    })
+  }
+
+  const deleteItem = (key) => {
+    setListOfItems((list) => {
+      return list.filter(({item}) => (item.key != key))
+    });
+  }
+
+  const changeItem = (key, data) => {
+    setListOfItems((list) => {
+      return [
+        {text: data.text, fav: data.fav, key: key},
+        ...list.filter(({item}) => (item.key != key))
+      ]
+    })
+  }
 
   return (
     <View>
-
+      <Pressable style={{padding: 100}} onPress={() => 
+      {
+        addItem(data={text:"TEST", fav:true, key: "1"}); console.log("Add=>", listOfItems)
+        changeItem(key="2", data={text:"CHTEST", fav:false}); console.log("Cng=>", listOfItems)
+        deleteItem(key="2"); console.log("Del=>", listOfItems)
+      }}>
+        <Text>Press me!</Text>
+      </Pressable>
     </View>
   );
+  
 }
 
 const styles = StyleSheet.create({
     
-});   
-
-
-
-
-
-{/* <FlatList data={listOfItems} renderItem={({item}) => (
-          <Element data={item}/>
-        )} /> */}
+});
--- components/Favorites.js
+++ components/Favorites.js
@@ -1,8 +1,12 @@
-import { StyleSheet, View } from "react-native-web"
-export default function Favorites() {
+import { StyleSheet, View, FlatList } from "react-native-web"
+import Element from "./ListElement"
+import { useState } from "react"
+
+export default function Favorites({list}) {
+
     return (
         <View>
-
+            <FlatList data={list.filter((el) => (el.fav))} renderItem={({item}) => <Element data={item} />} />
         </View>
     )
 }
@@ -10,3 +14,9 @@ export default function Favorites() {
 const styles= StyleSheet.create({
     
 })
+
+
+{/* <FlatList data={listOfItems} renderItem={({item}) => (
+          <Element data={item}/>
+        )} /> */}
+// () => {()}
